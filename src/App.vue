dde6d66f53bcc1547b3c1f6e1cbf51d87acb9923
huyphung1602 <huy.phung.sw@gmail.com>
2023-03-27 11:56:33 +0700

    Add packages: - Add tailwind CSS - Add d3 - Update the default layout

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,33 @@
 <template>
-  <h3>D3 Playground</h3>
-  <MonacoEditor
-    theme="nightOwl"
-    :options="options"
-    language="javascript"
-    :width="800"
-    :height="600"
-    v-model:value="playgroundCode"
-  ></MonacoEditor>
+  <div class="container mx-auto">
+    <p class="text-2xl py-8">D3 Playground</p>
+    <button
+      class="bg-violet-500 hover:bg-violet-600 focus:outline-none focus:ring focus:ring-violet-300 active:bg-violet-700 px-5 py-2 text-sm leading-5 rounded-md font-semibold text-white my-4"
+      @click="applyScript(playgroundCode)"
+    >
+      Run
+    </button>
+    <div class="flex flex-row">
+      <div class="basis-1/2">
+        <MonacoEditor
+          theme="nightOwl"
+          :options="options"
+          language="javascript"
+          :height="900"
+          v-model:value="playgroundCode"
+        />
+      </div>
+      <div
+        class="basis-1/2 border rounded-md shadow ml-4 p-8"
+      >
+        <div id="d3-customized-content">
+        <component :is="'script'">
+          {{ playgroundCode }}
+        </component>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -23,6 +43,11 @@ const options = {
   tabSize: 2,
 };
 const playgroundCode = ref('');
+const applyScript = (scriptCode : string) : void => {
+  console.log(scriptCode);
+  const result = eval(scriptCode);
+  console.log(result);
+};
 </script>
 
 <style scoped></style>
