b34e564d4fdc534d5c52302f365020d93d119bbf
huyphung1602 <huy.phung.sw@gmail.com>
2023-04-09 19:34:58 +0700

    Update bar_chart.js example

--- d3_examples/bar_chart.js
+++ d3_examples/bar_chart.js
@@ -15,24 +15,72 @@ d3.csv('src/data/1.csv', d => {
 });
 
 const createViz = (myData) => {
+  const svgWidth = 600;
+  const svgHeight = 700;
   const selection = d3
     .select('.d3-content');
   const svg = selection
     .append('svg')
-    .attr('viewBox', '0 0 1200 1600');
-  const barHeight = 20;  
+    .attr('viewBox', `0 0 ${svgWidth} ${svgHeight}`);
+  const defaultColor = '#39B5E0';
+  const xScale = d3.scaleLinear()
+    .domain(d3.extent(myData, d => d.count))
+    .range([0, 450]);
+  const yScale = d3.scaleBand()
+    .domain(myData.map(d => d.technology))
+    .range([0, svgHeight])
+    .paddingInner(0.2);
+
+  // svg
+  //   .selectAll('rect')
+  //   .data(myData)
+  //   .join('rect')
+  //     .attr('class', d => {
+  //       console.log(d);
+  //       return `bar bar-${d.technology}`;
+  //     })
+  //     .attr('width', d => xScale(d.count))
+  //     .attr('height', yScale.bandwidth())
+  //     .attr('x', '100')
+  //     .attr('y', d => yScale(d.technology))
+  //     .attr('fill', defaultColor);
+
+  const barAndLabel = svg
+    .selectAll('g')
+      .data(myData)
+      .join('g')
+      .attr('transform', d => `translate(0, ${yScale(d.technology)})`);
+
+  barAndLabel
+    .append("rect")
+      .attr("width", d => xScale(d.count))
+      .attr("height", yScale.bandwidth())
+      .attr("x", 100)
+      .attr("y", 0)
+      .attr("fill", d => d.technology === 'D3.js' ? 'yellowgreen': defaultColor);
+
+  barAndLabel
+    .append('text')
+      .text(d => d.technology)
+      .attr('x', 96)
+      .attr('y', 12)
+      .attr('text-anchor', 'end')
+      .style('font-family', 'sans-serif')
+      .style('font-size', '11px');
+
+  barAndLabel
+    .append('text')
+      .text(d => d.count)
+      .attr('x', d => 100 + xScale(d.count) + 4)
+      .attr('y', 12)
+      .style('font-family', 'sans-serif')
+      .style('font-size', '9px');
 
   svg
-    .selectAll('rect')
-    .data(myData)
-    .join('rect')
-      .attr('class', d => {
-        console.log(d);
-        return `bar bar-${d.technology}`;
-      })
-      .attr('width', d => d.count)
-      .attr('height', barHeight)
-      .attr('x', 0)
-      .attr('y', (d, i) => (barHeight + 4) * i)
-      .attr('fill', '#0d88e6');
+    .append('line')
+      .attr('x1', 100)
+      .attr('y1', 0)
+      .attr('x2', 100)
+      .attr('y2', svgHeight)
+      .attr('stroke', 'black');
 };
